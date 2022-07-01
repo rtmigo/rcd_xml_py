e4e0a8eeba8e94a346637136ebae9ec50275a750
Artem IG <ortemeo@gmail.com>
2022-07-01 18:56:23 +0300

    upd

--- rcd_xml/__init__.py
+++ rcd_xml/__init__.py
@@ -1,3 +1,6 @@
+from ._obsolete import removeXmlNamespace, htmlFileToTree, htmlBytesToTree, \
+    htmlCodeToTree, htmlAnchorToLink, findXpath, findXpathOne, listChildNodes, \
+    innerText, isText, isComment, innerText_old, isElement, findCss, \
+    removeTrailingSpaces, treeToFile, xmlFileToTree, xmlFileToTreeNoNS
 from ._remove_xmlns import remove_xmlns
-from ._obsolete import removeXmlNamespace, htmlFileToTree, htmlBytesToTree, htmlCodeToTree, htmlAnchorToLink, findXpath, findXpathOne, listChildNodes
 from ._rxml import *  # todo
